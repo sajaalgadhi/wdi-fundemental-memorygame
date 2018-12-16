1c94c17d9e0b42461ac4eddf54781853c5c71fc5
sajaalgadhi <45770348+sajaalgadhi@users.noreply.github.com>
2018-12-16 23:03:44 +0300

    Add files via upload

---
+++ wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -0,0 +1,82 @@
+ var cards = [
+{
+rank: "gueen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png'
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png'
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'images/king-of-diamonds.png'
+}
+ ];
+
+
+ var cardsInPlay=[];
+ 
+
+ var checkForMatch=function(){
+
+document.setAttribute('src','cards[cardId].cardImage');
+
+ 	 if (cardsInPlay[0] === cardsInPlay[1]) {
+       alert("You found a match!");
+}      else {
+       alert("Sorry, try again.");
+}
+
+ }
+
+
+ var flipCard=function(){
+
+cardId=this.getAttribute('data-id');
+console.log("user flipped"+cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage);
+
+ console.log(cards[cardId].suit);
+ 
+ console.log(cards[cardId].cardImage);
+
+
+
+
+
+checkForMatch();
+
+
+
+
+ 
+ }
+ 
+
+
+var createBoard=function(){
+
+
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+    
+}
+}
+createBoard();
+
+
+
